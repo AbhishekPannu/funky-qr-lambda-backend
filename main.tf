18788b7ad317f125369c0515c602211716b3dc4c
Abhishek  Pannu <abhishekpannu96@gmail.com>
2025-08-06 16:25:04 +0530

    tf code for creating s3 and dynamodb table for storing terraform state data

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "funky-qr-lambda-backend"
+    versioning {
+        enabled = true
+    }
+    acl = "private"
+    tags {
+        Name = "Terraform State Bucket"
+    }
+}
+
+resource "aws_dynamodb_table" "terraform_state_locking" {
+    name = "funky-qr-state-locking"
+    billing_mode = "PAY_PER_REQUESt"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags {
+        Name = "Terraform State Locking Table"
+    }
+}
